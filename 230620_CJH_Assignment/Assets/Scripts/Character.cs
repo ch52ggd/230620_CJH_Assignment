c1b659f7ad83df96d79a0de1c3a29fab61c97296
ch52ggd <136698331+ch52ggd@users.noreply.github.com>
2023-06-27 01:33:02 +0900

    수정
    수정

--- 230620_CJH_Assignment/Assets/Scripts/Character.cs
+++ 230620_CJH_Assignment/Assets/Scripts/Character.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -10,16 +11,54 @@ public class Character
     private int hp;
     private int atk;
 
+    public string Name { get { return name; } set { name = value; } }
+    public string Job { get { return job; } set { job = value; } }
+    public int Hp { get { return hp; } set { hp = value; } }
+    public int Atk { get { return atk; } set { atk = value; } }
+
     public Character(string name, string job, int hp, int atk)
     {
-        name = this.name;
-        job = this.job;
-        hp = this.hp;
-        atk = this.atk;
+        Name = name;
+        Job = job;
+        Hp = hp;
+        Atk = atk;
     }
+
+
 
     public void GetInfo()
     {
         Debug.Log($"Name : {this.name} Job : {this.job} HP : {this.hp} Atk : {this.atk}");
+    }
+
+
+
+    public void CreateCharacter(Character character)
+    {
+        /*
+        character.Name = name;
+        character.Job = job;
+        character.Hp = hp;
+        character.Atk = atk;
+        */
+    }
+
+
+
+    public void RemoveCharacter(int index)
+    {
+        
+    }
+
+
+
+    public void PrintAllCharacter()
+    {
+        int a = 0;
+        while (a < 5)
+        {
+            //GetInfo() ???
+            a++;
+        }
     }
 }
--- 230620_CJH_Assignment/Assets/Scripts/Main.cs
+++ 230620_CJH_Assignment/Assets/Scripts/Main.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 
 public class Main : MonoBehaviour
@@ -17,30 +18,28 @@ public class Main : MonoBehaviour
 
 
 
-        List<Character> c;
+        List<Character> character = new List<Character>();
 
-        c.Add(new Character("이름1", "직업1", 10, 10));
+        /*
+        character.Add(new Character("이름1", "직업1", 10, 10));
+        character.Add(new Character("이름2", "직업2", 20, 20));
+        character.Add(new Character("이름3", "직업3", 30, 30));
+        character.Add(new Character("이름4", "직업4", 40, 40));
+        character.Add(new Character("이름5", "직업5", 50, 50));
 
+        
+        int i = 0;
+        while(i < character.Count)
+        {
+            character[i].GetInfo();
+            i++;
+        }
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void CreateCharacter()
-    {
-
-    }
-
-    void RemoveCracter()
-    {
-
-    }
-
-    void PrintAllCharacter()
-    {
-
     }
 }
